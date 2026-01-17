2513612563c11e504dbfe62a704d746f78b0862e
Nerfisuniquee <110476522+Nerfisuniquee@users.noreply.github.com>
2026-01-17 18:23:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-export default {
-  async search(query) {
-    return [
-      {
-        id: "test-1",
-        title: "Test Anime Entry",
-        type: "torrent",
-        magnet: "magnet:?xt=urn:btih:0123456789ABCDEF0123456789ABCDEF01234567",
-        seeders: 1,
-        leechers: 0,
-        size: 0
-      }
-    ];
+export default [
+  {
+    id: "hayase-test-source",
+    name: "Hayase Test Source",
+    type: "torrent",
+
+    async search(query) {
+      return [
+        {
+          id: "test-1",
+          title: "Test Anime Entry",
+          magnet: "magnet:?xt=urn:btih:0123456789ABCDEF0123456789ABCDEF01234567",
+          seeders: 1,
+          leechers: 0,
+          size: 0
+        }
+      ];
+    }
   }
-};
+];
