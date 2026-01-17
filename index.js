273c75c85f6fba034156d1242dc610e4e4a82b9e
Nerfisuniquee <110476522+Nerfisuniquee@users.noreply.github.com>
2026-01-17 18:22:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ export default {
   async search(query) {
     return [
       {
+        id: "test-1",
         title: "Test Anime Entry",
-        url: "magnet:?xt=urn:btih:0123456789ABCDEF0123456789ABCDEF01234567",
-        type: "torrent"
+        type: "torrent",
+        magnet: "magnet:?xt=urn:btih:0123456789ABCDEF0123456789ABCDEF01234567",
+        seeders: 1,
+        leechers: 0,
+        size: 0
       }
     ];
   }
