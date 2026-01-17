0d92e66dea6033ece317bab6d7000b05ae31af91
Nerfisuniquee <110476522+Nerfisuniquee@users.noreply.github.com>
2026-01-17 18:19:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+export default {
+  async search(query) {
+    return [
+      {
+        title: "Test Anime Entry",
+        url: "magnet:?xt=urn:btih:0123456789ABCDEF0123456789ABCDEF01234567",
+        type: "torrent"
+      }
+    ];
+  }
+};
